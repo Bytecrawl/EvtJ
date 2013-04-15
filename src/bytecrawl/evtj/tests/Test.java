7890d510e7bf9c8bf6efdd43a840db8946bf8c77
Àlex Vinyals <alevinval@gmail.com>
2013-04-15 11:53:40 +0200

    Added JUnit Tests

---
+++ src/bytecrawl/evtj/tests/Test.java
@@ -0,0 +1,34 @@
+package bytecrawl.evtj.tests;
+import static org.junit.Assert.*;
+import bytecrawl.evtj.SocketEngine;
+import bytecrawl.evtj.server.EvtJServer;
+
+
+public class Test {
+	
+	public void sleep() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@org.junit.Test
+	public void ServerInit() {
+		
+		EvtJServer server = new EvtJServer(4444);
+		server.start();
+		
+		SocketEngine se1 = new SocketEngine("127.0.0.1", 4444);
+		sleep();
+		assertEquals("Server initialized", server.getConnectedClients(), 1);
+		
+		se1.close();
+		sleep();
+		
+		assertEquals("Disconnection", server.getConnectedClients(), 0);
+	}
+
+}
