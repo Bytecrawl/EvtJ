5965dda90f7700879cc8e033b6a55cad817be651
Àlex Vinyals <alevinval@gmail.com>
2013-05-16 11:38:52 +0200

    EvtJModule improvement.
    Now modules support onPause(), onResume(), onStop(), onStart()
    callbacks.

--- src/bytecrawl/evtj/server/EvtJModule.java
+++ src/bytecrawl/evtj/server/EvtJModule.java
@@ -3,5 +3,13 @@ package bytecrawl.evtj.server;
 public interface EvtJModule {
 
 	public EvtJModuleWorker getWorker();
-	
+
+	public void onPause();
+
+	public void onResume();
+
+	public void onStart();
+
+	public void onStop();
+
 }
