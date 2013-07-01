ec162dd07c856982a8ff5610be0c1ef35564a251
Àlex Vinyals <alevinval@gmail.com>
2013-07-01 21:15:17 +0200

    travis

---
+++ tests/bytecrawl/evtj/tests/TestSuite.java
@@ -0,0 +1,12 @@
+package bytecrawl.evtj.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   EvtJServerTests.class
+})
+public class TestSuite {
+	
+}  	
